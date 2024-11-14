fd7394811dd944fdd57efda04057c91566031c3d
mikasm-sys <matko.mikas@gmail.com>
2024-11-14 22:25:12 +0100

    update 1. funkcije + refresh .json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date
 
 # TODO: dodati type hinting na sve funkcije
 
@@ -35,7 +36,39 @@ def create_new_offer(offers, products, customers):
     # Omogućite unos kupca
     # Izračunajte sub_total, tax i total
     # Dodajte novu ponudu u listu offers
-    pass
+
+    ids = [offer['offer_number'] for offer in offers]
+
+    while True:
+        offer_number = max(ids) + 1
+        print(f"Broj ponude je -> {offer_number}")
+        customer = input(f"Unesite ime / naziv kupca: ")
+        date_now = date.today()
+        print(f"Današnji datum -> {date_now}")
+        print(f"-------------------------------\n"
+              f"LISTA PROIZVODA: ||\n"
+              f"-----------------ˇˇ------------")
+        for product in products:
+            print(f"ID proizvoda -> {product['id']}\n"
+                  f"Naziv proizvoda -> {product['name']}\n"
+                  f"Opis proizvoda -> {product['description']}\n"
+                  f"Cijena proizvoda -> {product['price']}\n"
+                  f"-----------------------------------------")
+            
+        product_id_offer = input(f"Unesite ID proizvoda za kupiti: ")
+        product_ids = [id for id in product_id_offer.split(',')]
+        product_ids.append(product_id_offer)
+
+
+        if len(product_ids) > 1:
+            print(product_ids)
+        elif len(product_ids) == 1:
+            product_id = product_ids[0]
+            print(product_id)
+        else:
+            odabir2 = input(f"Nisi odabrao niti jednu ponudu. Želiš li nastaviti sa stvaranjem ponude? da/ne ")
+            if odabir2.lower != 'da':
+                break             
 
 
 # TODO: Implementirajte funkciju za upravljanje proizvodima.
@@ -46,7 +79,63 @@ def manage_products(products):
     # Omogućite korisniku izbor između dodavanja ili izmjene proizvoda
     # Za dodavanje: unesite podatke o proizvodu i dodajte ga u listu products
     # Za izmjenu: selektirajte proizvod i ažurirajte podatke
-    pass
+    ids = [product['id'] for product in products]
+    
+    odabir = input(f"Želiš li postaviti novi proizvod (1) ili izmijeniti postojeći (2)? ")
+    if odabir == "1":
+        while True:
+            id = max(ids) + 1
+            name = input(f"Unesi naziv proizvoda: ").strip()
+            description = input(f"Unesite opis proizvoda: ").strip()
+            price = input(f"Unesite cijenu proizvoda: ")
+            while not price.isdigit():
+                    print(f"Nedopušten znak. Probaj opet")
+                    price = input(f"Unesite cijenu proizvoda: ")
+            ponuda = {
+                'id' : id,
+                'name' : name,
+                'description' : description,
+                'price' : price
+            }
+            products.append(ponuda)
+            print(f"Dodan je novi proizvod:\n"
+                f"ID -> {id}\n"
+                f"Naziv proizvoda -> {name}\n"
+                f"Opis proizvoda -> {description}\n"
+                f"Cijena -> {price}")
+            odabir_2 = input(f"Želiš li dodati još jedan proizvod? (da/ne): ")
+            if odabir_2.lower() != 'da':
+                break            
+
+
+    elif odabir == "2":
+        while True:
+            odabir_2 = int(input(f"Odaberi ID postojećeg proizvoda kojeg želiš promijeniti: "))
+            while odabir_2 not in ids:
+                print(f"Nepostojeći proizvod. Probaj opet.")
+                odabir_2 = int(input(f"Odaberi ID postojećeg proizvoda kojeg želiš promijeniti: "))
+            print(f"Odabrani proizvod -> {products[odabir_2]}")
+            print(f"Tijekom dodavanja novog ID, on ne smije biti jedan od sljedećih - {ids}")
+            novi_id = input(f"Unesi novi ID proizvoda: ")
+            name = input(f"Unesi naziv proizvoda: ").strip()
+            description = input(f"Unesite opis proizvoda: ").strip()
+            price = input(f"Unesite cijenu proizvoda: ")
+            while not price.isdigit():
+                    print(f"Nedopušten znak. Probaj opet")
+                    price = input(f"Unesite cijenu proizvoda: ")
+            ponuda = {
+                'id' : novi_id,
+                'name' : name,
+                'description' : description,
+                'price' : price
+            }
+            for product in products:
+                if product[id] == novi_id:
+                    product == ponuda
+            odabir_2 = input(f"Želiš li promijeniti još jedan proizvod? (da/ne): ")
+            if odabir_2.lower() != 'da':
+                break  
+
 
 
 # TODO: Implementirajte funkciju za upravljanje kupcima.
@@ -56,7 +145,39 @@ def manage_customers(customers):
     """
     # Za dodavanje: omogući unos imena kupca, emaila i unos VAT ID-a
     # Za pregled: prikaži listu svih kupaca
-    pass
+    odabir = input("Želiš li dodati novog kupca (1) ili vidjeti postojeće (2)? ")
+    if odabir == '1':
+        while True:
+            name = input(f"Unesi ime / naziv kupca: ").strip()
+            email = input(f"Unesi e-mail kupca: ").strip()
+            vat_id = input(f"Unesi porezni ID kupca: ")
+            while len(vat_id) != 10 or not vat_id.isdigit():
+                print(f"Ili je broj znamenki netočan ili je upisan nedopušten znak. Probaj opet")
+                vat_id = input(f"Unesi porezni ID kupca: ")
+            novi_kupac = {
+                'name' : name,
+                'email' : email,
+                'vat_id' : vat_id
+            }
+            customers.append(novi_kupac)
+            print(f"Ime / naziv kupca -> {novi_kupac['name']}\n"
+                  f"E-mail -> {novi_kupac['email']}\n"
+                  f"Porezni ID -> {novi_kupac['vat_id']}")
+            odabir_2 = input(f"Želiš li dodati još jednog kupca? (da/ne): ")
+            if odabir_2.lower() != 'da':
+                break
+        
+    elif odabir == '2':
+        try:
+            for customer in customers:
+                print(f"Ime / naziv kupca -> {customer['name']}\n"
+                      f"E-mail -> {customer['email']}\n"
+                      f"Porezni ID -> {customer['vat_id']}\n"
+                      f"-----------------------------------")
+        except Exception as e:
+            print(f"Greška -> {e}")
+    else:
+        print(f"Nepostojeća opcija. Probaj opet")
 
 
 # TODO: Implementirajte funkciju za prikaz ponuda.
